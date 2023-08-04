d0d0e00f013b64f2f6edbc46086ad43044e982aa
hassantimothy <hassantimothy422@gmai.com>
2023-08-04 21:24:29 +0300

    Implemented a Rock-Paper-Scissors game in JavaScript. Added functions for getting the computer's choice, playing a round, and running a 5-round game with score tracking. User inputs are obtained through prompts, and the winner or loser of the game is reported at the end.

--- script.js
+++ script.js
@@ -1,9 +1,62 @@
 function getComputerChoice() {
-    const values = ['Rock', 'Paper', 'Scissors'];
+    const values = ['rock', 'paper', 'scissors'];
     const randomIndex = Math.floor(Math.random() * values.length);
     const answer = values[randomIndex];
 
-    console.log(answer);
+    return answer;
+}
+const computerChoice = getComputerChoice();
+console.log('Computer chose:', computerChoice);
+
+function playRound(playerSelection, computerSelection) {
+    playerSelection = playerSelection.toLowerCase();
+
+    if (playerSelection === 'rock' && computerSelection === 'scissors') {
+        return 'You win! Rock beats scissors';
+    } else if (playerSelection === 'rock' && computerSelection === 'rock') {
+        return 'Draw';
+    } else if (playerSelection === 'rock' && computerSelection === 'paper') {
+        return 'You lose! Paper beats rock';
+    } else if (playerSelection === 'scissors' && computerSelection === 'rock') {
+        return 'You lose! Rock beats scissors';
+    } else if (playerSelection === 'scissors' && computerSelection === 'paper') {
+        return 'You win! Scissors beats paper';
+    } else if (playerSelection === 'scissors' && computerSelection === 'scissors') {
+        return 'Draw';
+    } else if (playerSelection === 'paper' && computerSelection === 'rock') {
+        return 'You win! Paper beats rock';
+    } else if (playerSelection === 'paper' && computerSelection === 'scissors') {
+        return 'You lose! Scissors beats paper';
+    } else if (playerSelection === 'paper' && computerSelection === 'paper') {
+        return 'Draw';
+    }
 }
 
-getComputerChoice(); // Call the function to get a random choice
+function game() {
+    let playerScore = 0;
+    let computerScore = 0;
+
+    for (let i = 1; i <= 5; i++) {
+        const playerSelection = prompt(`Round ${i}: Enter your choice (rock, paper, or scissors):`).toLowerCase();
+        const computerSelection = getComputerChoice();
+        
+        const result = playRound(playerSelection, computerSelection);
+        console.log(result);
+
+        if (result.includes('win')) {
+            playerScore++;
+        } else if (result.includes('lose')) {
+            computerScore++;
+        }
+    }
+
+    if (playerScore > computerScore) {
+        console.log('Congratulations! You win the game.');
+    } else if (computerScore > playerScore) {
+        console.log('Sorry, you lose the game.');
+    } else {
+        console.log('It\'s a tie! The game is a draw.');
+    }
+}
+
+game();
